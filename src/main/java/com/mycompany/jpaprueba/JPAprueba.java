a36a0b443a5ebb35d8c367fb5328f49fabe76d13
Diego <diegovegaprogra@gmail.com>
2026-01-06 11:21:16 -0600

    -- JPAproyecto --

---
+++ src/main/java/com/mycompany/jpaprueba/JPAprueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jpaprueba;
+
+/**
+ *
+ * @author diego
+ */
+public class JPAprueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
